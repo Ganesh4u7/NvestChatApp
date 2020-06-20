f0053293ea2a68f6a382aced2f6a4330b2ae6551
Ganesh Kumar <phanikumar2791@gmail.com>
2020-06-21 02:13:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+var path = require('path');
+var express = require('express');
+var http = require('http');
+var mongoose = require('mongoose');
+var socketio = require('socket.io');
+var formatMessage = require('./utils/messages');
+var {userJoin,getCurrentUser,userLeave,getRoomUsers} = require('./utils/users');
+
+
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+var botName = 'some name';
+
+app.use(express.static(path.join(__dirname,'public')));
+
+io.on('connection',socket=>{
+    console.log('new connection made');
+
+    socket.on('joinRoom',({username,room})=>{
+        var user = userJoin(socket.id,username,room);
+
+        console.log(user.room);
+
+        socket.join(user.room);
+
+        socket.emit('message',formatMessage(botName,'Welcome to chat room'));
+
+        socket.broadcast.to(user.room).emit('message',
+            formatMessage(botName,`${user.username} has joined`));
+
+        io.to(user.room).emit('roomUsers',{
+            room:user.room,
+            users:getRoomUsers(user.room)
+        });
+    });
+
+
+
+    socket.on('chatMessage',(msg)=>{
+        var user = getCurrentUser(socket.id);
+        console.log(msg);
+        io.to(user.room).emit('message',formatMessage(user.username,msg));
+    })
+    socket.on('disconnect',()=>{
+        var user = userLeave(socket.id);
+        if(user){
+
+            io.to(user.room).emit('message',formatMessage(botName,`${user.username} disconnected`));
+            io.to(user.room).emit('roomUsers',{
+                room:user.room,
+                users:getRoomUsers(user.room)
+            });
+        }
+    });
+})
+
+
+var port = 4000 || process.env.port;
+
+server.listen(port,function (err,res) {
+if(err){console.log(err)}
+else{ console.log('server connected to port:'+port)}
+});
