25f0f5da1f40eb5c34ea7382129ff45e1fd0dc28
Ganesh4u7 <42564733+Ganesh4u7@users.noreply.github.com>
2020-06-21 02:42:44 +0530

    Add files via upload

--- utils/messages.js
+++ utils/messages.js
@@ -1,9 +1,10 @@
 var moment =require('moment');
 
-function formatMessage(username,text) {
+function formatMessage(username,text,index) {
     return {
         username,
         text,
+        index,
         date: moment().format('h:mm a')
     }
 }
--- utils/users.js
+++ utils/users.js
@@ -1,9 +1,29 @@
+var mongoose = require('mongoose');
+var usersSchema = require('../models/userSchema');
+var usersData = mongoose.model('userData',usersSchema);
+
 var users =[];
 
-function userJoin(id,username,room){
-   var user = {id,username,room};
-   users.push(user);
-   return user;
+
+function userEntered(id,username,rooms) {
+var user = {id,username,rooms};
+users.push(user);
+//   console.log(users[0].rooms);
+// console.log('hi');
+}
+
+function userJoin(id,username,room,index){
+   users.find(user => {
+     if(user.username === username){
+       user.rooms[index].status = 1;
+       //console.log(user);
+
+     }
+   });
+
+ return  users.find(user => user.username === username);
+
+
 }
 
 function getCurrentUser(id) {
@@ -12,6 +32,7 @@ function getCurrentUser(id) {
 
 function userLeave(id){
     var index = users.findIndex(user => user.id === id);
+    console.log(index);
 
     if(index !== -1){
         return users.splice(index,1)[0];
@@ -20,10 +41,16 @@ function userLeave(id){
 function getRoomUsers(room){
     return users.filter(user => user.room === room);
 }
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
 
 module.exports = {
     userJoin,
     getCurrentUser,
     userLeave,
-    getRoomUsers
+    getRoomUsers,
+    userEntered,
+    validateEmail
 };
